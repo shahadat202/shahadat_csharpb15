643ea4e1adf089fcc7900d71de44daf8835788da
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-09 19:24:10 +0600

    modified practiceProject

--- practiceProject/practiceProject/Program.cs
+++ practiceProject/practiceProject/Program.cs
@@ -1,11 +1,28 @@
 ﻿namespace practiceProject
 {
-    class Program
+    class program
     {
         static void Main(string[] args)
         {
-            int n = 10;
-            Console.WriteLine(n);
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch (n)
+            {
+                case 0:
+                    Console.WriteLine($"{n} is less than 3");
+                    break;
+                case 1:
+                    Console.WriteLine($"{n} is less than 3");
+                    break;
+                case 2:
+                    Console.WriteLine($"{n} is less than 3");
+                    break;
+                case 3:
+                    Console.WriteLine($"{n} is equal to 3");
+                    break;
+                default: 
+                    Console.WriteLine($"{n} is greater than 3");
+                    break;
+            }
         }
     }
 }
