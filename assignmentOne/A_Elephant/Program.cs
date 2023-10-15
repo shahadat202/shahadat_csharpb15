70369fcdda05b6d24abe979968da7c16e5599d85
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:53:46 +0600

    solved another

---
+++ assignmentOne/A_Elephant/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Specialized;
+
+namespace A_Elephant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n <= 5)
+            {
+                Console.WriteLine(1);
+            }
+            else if (n % 5 == 0)
+            {
+                Console.WriteLine(n / 5);
+            }
+            else
+            {
+                Console.WriteLine((n / 5) + 1);
+            }
+        }
+    }
+}
