b231fdb3537457ce84246c5ace8ba8e333e3be39
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:41:30 +0600

    solved 4th problem

---
+++ assignmentOne/A_Petya_And_Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace A_Petya_And_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            a = a.ToLower();
+            b = b.ToLower();
+
+            int result = string.Compare(a, b);
+
+            if (result < 0)
+            {
+                Console.WriteLine(-1);
+            }
+            else if (result == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(1);
+            }
+        }
+    }
+}
