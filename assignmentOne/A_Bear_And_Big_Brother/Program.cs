432c79fe79f975915a27307431a4e05b47fcf900
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:51:38 +0600

    solved another

---
+++ assignmentOne/A_Bear_And_Big_Brother/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace A_Bear_And_Big_Brother
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l, b;
+            string[] input = Console.ReadLine().Split();
+            l = int.Parse(input[0]);
+            b = int.Parse(input[1]);
+
+            int cnt = 0;
+            while (l <= b)
+            {
+                l *= 3;
+                b *= 2;
+                cnt++;
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
