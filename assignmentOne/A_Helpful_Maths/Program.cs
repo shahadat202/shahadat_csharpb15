7f19588546d881eff1ea86df7845f9203c5fa05b
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:46:13 +0600

    solved another

---
+++ assignmentOne/A_Helpful_Maths/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace A_Helpful_Maths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char[] chars = s.ToCharArray();
+
+            for (int i = 0; i < chars.Length - 1; i += 2)
+            {
+                for (int j = 0; j < chars.Length - 1; j += 2)
+                {
+                    if (chars[j] > chars[j + 2])
+                    {
+                        char temp = chars[j];
+                        chars[j] = chars[j + 2];
+                        chars[j + 2] = temp;
+                    }
+                }
+            }
+            string result = new string(chars);
+            Console.WriteLine(result);
+        }
+    }
+}
