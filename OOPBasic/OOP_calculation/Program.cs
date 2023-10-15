264de18fc0020638e190d9a8dfc8e7bb01840f5a
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 16:26:40 +0600

    modified OOPBasic

---
+++ OOPBasic/OOP_calculation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace practice
+{
+    class OOPBasic
+    {
+
+        static void Main(string[] args)
+        {
+            calculation calculation = new calculation();
+
+
+
+            int sum1 = calculation.Add(5, 6);
+            Console.WriteLine("Sum of a and b is: " + sum1);
+
+            double sum2 = calculation.Add(5.5, 7.6);
+            Console.WriteLine("Sum of c and d is: " + sum2);
+
+            string s = calculation.Add("Shahadat ", "Hossain");
+            Console.WriteLine("Concatenated string: " + s);
+        }
+    }
+}
+
---
+++ OOPBasic/OOP_calculation/calculation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    class calculation
+    {
+        // Method to add two integers
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        // Method to add two double
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        // Method to concatenate two strings
+        public string Add(string x, string y)
+        {
+            return x + y;
+        }
+
+    }
+}
