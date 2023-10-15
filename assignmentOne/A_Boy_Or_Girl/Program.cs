e30e833b0ca7be2d13eba93683373c1d24789143
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:48:36 +0600

    solved another

---
+++ assignmentOne/A_Boy_Or_Girl/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace A_Boy_Or_Girl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            HashSet<char> set = new HashSet<char>();
+
+            foreach (char c in a)
+            {
+                set.Add(c);
+            }
+            if (set.Count % 2 == 1)
+            {
+                Console.WriteLine("IGNORE HIM!");
+            }
+            else
+            {
+                Console.WriteLine("CHAT WITH HER!");
+            }
+        }
+    }
+}
