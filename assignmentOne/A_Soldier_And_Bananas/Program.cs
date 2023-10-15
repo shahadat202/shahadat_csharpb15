e1ac99de66c372694276808aa685ffbb4b15a9ad
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:50:39 +0600

    solved another

---
+++ assignmentOne/A_Soldier_And_Bananas/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace A_Soldier_And_Bananas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, n, w;
+            string[] input = Console.ReadLine().Split();
+            k = int.Parse(input[0]);
+            n = int.Parse(input[1]);
+            w = int.Parse(input[2]);
+
+            int sum = 0;
+            for (int i = 1; i <= w; i++)
+            {
+                sum += (i * k);
+            }
+            if (sum <= n)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(sum - n);
+            }
+        }
+    }
+}
