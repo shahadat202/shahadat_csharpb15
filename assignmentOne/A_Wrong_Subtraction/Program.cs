dbdac6b21189c73beae4107b3642f27e5dd4189b
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:52:36 +0600

    solved another

---
+++ assignmentOne/A_Wrong_Subtraction/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace A_Wrong_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(input[0]);
+            int k = int.Parse(input[1]);
+
+            while (k > 0)
+            {
+                if (n % 10 > 0)
+                {
+                    n--;
+                }
+                else
+                {
+                    n /= 10;
+                }
+                k--;
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
