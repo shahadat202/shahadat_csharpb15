8dec6ea20ac2bde5f7303f951cc1e2e5ee24c793
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 09:01:12 +0600

    solved another

---
+++ assignmentOne/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace A_HQ9_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            bool found = false;
+
+            foreach (char c in s)
+            {
+                if (c == 'H' || c == 'Q' || c == '9')
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (found)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
