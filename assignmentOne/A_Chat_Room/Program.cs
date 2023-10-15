33ae313644d4469ec3e0b50e894170b3be415642
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:58:00 +0600

    solved another

---
+++ assignmentOne/A_Chat_Room/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class A_Chat_Room
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        string hello = "hello";
+
+        int i = 0, j = 0;
+        while (i < s.Length && j < hello.Length)
+        {
+            if (s[i] == hello[j])
+            {
+                j++;
+            }
+            i++;
+        }
+
+        if (j == hello.Length)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
