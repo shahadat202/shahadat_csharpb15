7945c49b31556e5180e8b91cd26336b5939e6a1c
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:47:28 +0600

    solved another

---
+++ assignmentOne/A_Stones_On_The_Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace A_Stones_On_The_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            char[] chars = s.ToCharArray();
+
+            int ans = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (s[i] == s[i - 1]) ans++;
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
