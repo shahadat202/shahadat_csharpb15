6db263babcf440cdcc0e2a0e36f02bddc19fd11e
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:45:03 +0600

    solved another

---
+++ assignmentOne/A_String_Task/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace A_String_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            s = s.ToLower();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y')
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.Write($".{s[i]}");
+                }
+            }
+        }
+    }
+}
