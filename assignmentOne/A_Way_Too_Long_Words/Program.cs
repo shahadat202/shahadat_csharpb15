00d3c34b64e963573d5f46f168358d38195d1a21
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:34:16 +0600

    solved first problem

---
+++ assignmentOne/A_Way_Too_Long_Words/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace A_Way_Too_Long_Words
+{
+    internal class Program
+    {
+        static void solve()
+        {
+            string s = Console.ReadLine();
+            if (s.Length <= 10)
+            {
+                Console.WriteLine(s);
+            }
+            else
+            {
+                Console.WriteLine($"{s[0]}{s.Length - 2}{s[^1]}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t > 0)
+            {
+                solve();
+                t--;
+            }
+        }
+    }
+}
