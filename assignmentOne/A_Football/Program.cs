4b51dbc47de104c26197a342c1435ec64c2cc8ef
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:49:39 +0600

    solved another

---
+++ assignmentOne/A_Football/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace A_Football
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int cnt = 1, ans = 1;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    cnt++;
+                }
+                else
+                {
+                    ans = Math.Max(ans, cnt);
+                    cnt = 1;
+                }
+            }
+            ans = Math.Max(ans, cnt);
+            if (ans >= 7)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
