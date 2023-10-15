023efa2c816c9ada14503c97f41966e3dc07e6a9
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:38:03 +0600

    solved second problem

---
+++ assignmentOne/A_Bit++/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace A_Way_Too_Long_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            int cnt = 0;
+            while (t > 0)
+            {
+                string s = Console.ReadLine();
+                if (s[1] == '+') cnt++;
+                else cnt--;
+                t--;
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
