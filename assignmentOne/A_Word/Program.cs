de0af05c6bf4d4f51685f8e00ab1131c5cc53f5e
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-15 08:54:47 +0600

    solved another

---
+++ assignmentOne/A_Word/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace A_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int low = 0, up = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (Char.IsUpper(s[i]))
+                {
+                    up++;
+                }
+                else low++;
+            }
+            if (up > low)
+            {
+                s = s.ToUpper();
+            }
+            else
+            {
+                s = s.ToLower();
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
