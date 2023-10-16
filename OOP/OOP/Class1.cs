5756794f4c29b094c963acefa93bba8382d9dbe7
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-16 19:39:11 +0600

    added OOPBasics file

---
+++ OOP/OOP/Class1.cs
@@ -0,0 +1,47 @@
+﻿namespace OOPBasics
+{
+    class Person
+    {
+        private string firstName;
+        public string LastName { get; set; }
+
+        public string FirstName
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(firstName))
+                    return string.Empty;
+                return firstName;
+            }
+            set
+            {
+                if(!string.IsNullOrWhiteSpace(value))
+                    firstName = value;
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return firstName + " " + LastName;
+            }
+        }
+        public Person() : this(string.Empty, string.Empty)
+        {
+        }
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.LastName = lastName;
+        }
+        public string GetName(string separator)
+        {
+            string fullName = firstName + separator + LastName;
+            return fullName;
+        }
+        public string GetName(string[] separator)
+        {
+            return GetName(separator[0]);
+        }
+    }
+}
---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using OOPBasics;
+
+Person person = new Person("Shahadat", "Hossain");
+string name = person.GetName(new string[] { " " });
+Console.WriteLine(name);
+
+Person person2 = new Person("Saleh", "Ahmed");
+//person2.FirstName = "";
+//person2.LastName = "Hassan";
+string name2 = person2.GetName(" ");
+Console.WriteLine(name2);
+//Console.WriteLine(person2.FullName);
