3386397724ce8a6f9e3790cdc3c0713cf3554ff7
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-10-30 22:35:44 +0600

    solved task 1

--- src/Exam2/Task1/Program.cs
+++ src/Exam2/Task1/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace task
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char c = '*';
+
+            for (int i = 1; i <= n; i+=2)
+            {
+                int space = (n-i) / 2;
+                for (int j = 0; j < space; j++)
+                {
+                    Console.Write(' ');
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(c);
+                }
+                for (int j = 0; j < space; j++)
+                {
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+            for (int i = n - 2; i >= 1; i -= 2)
+            {
+                int space = (n - i) / 2;
+                for (int j = 0; j < space; j++)
+                {
+                    Console.Write(' ');
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(c);
+                }
+                for (int j = 0; j < space; j++)
+                {
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
