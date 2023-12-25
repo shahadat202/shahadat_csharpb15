6173a44dafae05a13c2ed1c126c5183d6ddd347e
MD Shahadat Hossain <sharman.bd0@gmail.com>
2023-12-25 23:05:18 +0600

    Task1

---
+++ Exam4/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+long byteSize = 5L * 1024 * 1024 * 1024;
+long fileSize = byteSize / 4;
+
+string path = "../../../../text.txt";
+if (!File.Exists(path))
+    File.Create(path);
+
+GenerateFile(path, fileSize);
+
+static void GenerateFile(string path, long fileSize)
+{
+    
+}
+
+
+Random random = new Random(DateTime.Now.Millisecond);
+
+for(int i = 0; i < 50; i++)
+{
+    Console.Write(GetRandomChar());
+}
+
+char GetRandomChar()
+{
+    return (char)random.Next('A', 'Z');
+}
