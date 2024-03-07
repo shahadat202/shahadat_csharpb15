e99f2a1be65f7eb8a811ec01cb2b4cbb5d93cc8d
MD Shahadat Hossain <sharman.bd0@gmail.com>
2024-03-07 16:02:07 +0600

    Completed Exam2 questions

--- src/Exam2/Task2/Program.cs
+++ src/Exam2/Task2/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+    static void solve()
+    {
+        string[] s = Console.ReadLine().Split();
+        int row = int.Parse(s[0]);
+        int col = int.Parse(s[1]);
+
+        char[,] chars = new char[row, col];
+
+        for (int i = 0; i < row; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            for (int j = 0; j < col; j++)
+            {
+                chars[i, j] = char.Parse(input[j]);
+            }
+        }
+
+        int mx = 0;
+        for (int i = 0; i < row; i++)
+        {
+            int cnt = 1;
+            for (int j = 0; j < col-1; j++)
+            {
+                if (chars[i, j] == chars[i, j + 1])
+                {
+                    cnt++;
+                    mx = Math.Max(mx, cnt);
+                }
+                else
+                {
+                    cnt = 1;
+                }
+            }
+            mx = Math.Max(mx, cnt);
+        }
+        Console.WriteLine(mx);
+    }
+    static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+        while (t-- > 0)
+        {
+            solve();
+        }
+    }
+}
--- src/Exam2/Task3/Program.cs
+++ src/Exam2/Task3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static string ModifyString(string s)
+    {
+        string cleanString = Regex.Replace(s, @"[^a-zA-Z0-9_ $]", "");
+
+        cleanString = cleanString.Replace('_', ' ');
+        cleanString = cleanString.Replace('$', ' ');
+
+        cleanString = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(cleanString.ToLower());
+
+        cleanString = Regex.Replace(cleanString, @"\s+", " ").Trim();
+
+        return cleanString;
+    }
+    static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+        while (t-- > 0)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(ModifyString(s));
+        }
+    }
+}
